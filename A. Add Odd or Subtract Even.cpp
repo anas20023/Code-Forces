802a996a5999da8295ea831593b64d8619bcfd5e
anas20023 <yorder07@gmail.com>
2024-04-17 04:08:52 +0600

    Refactor code in A. Add Odd or Subtract Even.cpp

--- A. Add Odd or Subtract Even.cpp
+++ A. Add Odd or Subtract Even.cpp
@@ -1,38 +1,41 @@
-#include <iostream>
+////////////////              ANAS IBN BELAL            /////////////////
+//                         Test  Case  Template                       ||  
+//                                                                    || 
+//                                                                    ||
+//      BANGLADESH  UNIVERSITY OF BUSINESS AND TECHNOLOGY             || 
+//                          INTAKE 52                                 ||
+//                      DEPARTMENT OF CSE                             ||
+//                                                                    ||  
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#include <bits/stdc++.h>
+#include <vector>
 #include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <map>
+#include <deque>
+#include <list>
+#include <limits.h>
 #define ll long long
+#define endl "\n"
 using namespace std;
+
+void Fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
 
 int main()
 {
+    Fast_io();
     ll tc;
     cin >> tc;
     while (tc--)
-    {
-        ll a, b, count = 0;
-        cin >> a >> b;
-        if (a == b)
-        {
-            cout << 0 << endl;
-            return 0;
-        }
-        ll tempa = a, i = 1, j = 2;
-        while (a != b)
-        {
-            // count++;
-            if (a + i == b)
-            {
-                count++;
-                break;
-            }
-            if (a - j == b)
-            {
-                count++;
-            }
-        }
-
-        cout << count << endl;
-    }
+    {}
 
     return 0;
 }
