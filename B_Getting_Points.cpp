e894d1c0e98a56ceb29251625a693c7a759e07ab
anas20023 <yorder07@gmail.com>
2024-05-14 20:51:08 +0600

    14/05-2024

---
+++ B_Getting_Points.cpp
@@ -0,0 +1,42 @@
+////////////////              ANAS IBN BELAL            /////////////////
+//                         Test  Case  Template                       ||
+//                                                                    ||
+//                                                                    ||
+//      BANGLADESH  UNIVERSITY OF BUSINESS AND TECHNOLOGY             ||
+//                          INTAKE 52                                 ||
+//                      DEPARTMENT OF CSE                             ||
+//                                                                    ||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+void Fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    Fast_io();
+    ll tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n, P, l, t;
+        cin >> n >> P >> l >> t;
+        while (n--)
+        {
+            if (P == l)
+            {
+                cout << "0\n";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
