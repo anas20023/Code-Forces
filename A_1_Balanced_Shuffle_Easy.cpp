aa26680728d0a094141e2608f8430bd27286baf4
anas20023 <yorder07@gmail.com>
2024-06-14 20:18:20 +0600

    Refactor A_1_Balanced_Shuffle_Easy.cpp for code optimization

--- A_1_Balanced_Shuffle_Easy.cpp
+++ A_1_Balanced_Shuffle_Easy.cpp
@@ -12,7 +12,6 @@
 #define endl "\n"
 #define Qtpie 2 * acos(0.0)
 using namespace std;
-
 void Fast_io()
 {
     ios_base::sync_with_stdio(false);
@@ -25,5 +24,21 @@ int main()
     Fast_io();
     string d;
     cin >> d;
+    vector<tuple<int, int, char>> a(d.size());
+    for (int i = 0, sum = 0; i < (int)d.size(); ++i)
+    {
+        a[i] = make_tuple(sum, -i, d[i]);
+        if (d[i] == '(')
+            ++sum;
+        else
+            --sum;
+    }
+    sort(a.begin(), a.end());
+    string ans;
+    for (const auto &t : a)
+    {
+        ans += get<2>(t);
+    }
+    cout << ans << '\n';
     return 0;
 }
