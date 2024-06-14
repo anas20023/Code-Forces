871051d1864d1296b4af4cd1b0b6a4beeb3efd54
anas20023 <yorder07@gmail.com>
2024-06-14 14:43:59 +0600

    1000

---
+++ A_Problem_Generator.cpp
@@ -0,0 +1,42 @@
+////////////////              ANAS IBN BELAL            /////////////////
+//                         Test  Case  Template                       ||
+//                                                                    ||
+//                                                                    ||
+//      BANGLADESH  UNIVERSITY OF BUSINESS AND TECHNOLOGY             ||
+//                          INTAKE 52                                 ||
+//                      DEPARTMENT OF CSE                             ||
+//                                                                    ||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define Qtpie 2 * acos(0.0)
+using namespace std;
+
+void Fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    Fast_io();
+    ll tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n, m;
+        cin >> n >> m;
+        string s;
+        cin >> s;
+        set<char> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            st.insert(s[i]);
+        }
+        cout << 7 - st.size() << endl;
+    }
+    return 0;
+}
