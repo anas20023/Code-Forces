750e25d655e2c69469ffa16fab2d8a2cf33a0f44
anas20023 <yorder07@gmail.com>
2024-06-20 10:03:22 +0600

    chore: Refactor A_Boring_Apartments.cpp for improved readability and efficiency

--- B_Sale.cpp
+++ B_Sale.cpp
@@ -1,5 +1,5 @@
 ////////////////              ANAS IBN BELAL            /////////////////
-//                         Test  Case  Template                       ||
+//                      Test Case Less Template                       ||
 //                                                                    ||
 //                                                                    ||
 //      BANGLADESH  UNIVERSITY OF BUSINESS AND TECHNOLOGY             ||
@@ -23,27 +23,21 @@ void Fast_io()
 int main()
 {
     Fast_io();
-    ll tc;
-    cin >> tc;
-    while (tc--)
+    int n, m, cnt = 0;
+    cin >> n >> m;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
     {
-        int c;
-        cin >> c;
-        int dgt = 0;
-        int mtdgt = ((c % 10) - 1) * 10;
-        while (c != 0)
-        {
-            dgt++;
-            c /= 10;
-        }
-        if (dgt == 2)
-            mtdgt += 1;
-        else if (dgt == 3)
-            mtdgt += 3;
-        else if (dgt == 4)
-            mtdgt += 6;
-        cout << dgt + mtdgt << endl;
+        int tmp;
+        cin >> tmp;
+        v.push_back(tmp);
     }
+    sort(v.begin(), v.end());
+    for (int i = 0; v[i] <= 0 && i < m; i++)
+    {
+        cnt += v[i];
+    }
+    cout << abs(cnt) << endl;
 
     return 0;
 }
