30024750a043c1fb076dd9ad60cd71ad160cdcef
Anas Ibn Belal <yorder07@gmail.com>
2024-08-31 01:23:59 +0600

    Refactor code to remove unnecessary files and improve readability and maintainability

---
+++ Discovering_Permutations.cpp
@@ -0,0 +1,50 @@
+////////////////              ANAS IBN BELAL            /////////////////
+//                         Test  Case  Template                       ||
+//                                                                    ||
+//                                                                    ||
+//      BANGLADESH  UNIVERSITY OF BUSINESS AND TECHNOLOGY             ||
+//                          INTAKE 52                                 ||
+//                      DEPARTMENT OF CSE                             ||
+//                                                                    ||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+void Fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+int main()
+{
+    Fast_io();
+    return 0;
+}
+
+//                         Test  Case  Template                       ||
+//                                                                    ||
+//                                                                    ||
+//      BANGLADESH  UNIVERSITY OF BUSINESS AND TECHNOLOGY             ||
+//                          INTAKE 52                                 ||
+//                      DEPARTMENT OF CSE                             ||
+//                                                                    ||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+void Fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+int main()
+{
+    Fast_io();
+    return 0;
+}
