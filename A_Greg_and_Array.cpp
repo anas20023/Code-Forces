94b80b1774ffe4e318df791e484c12804cfd7174
Anas Ibn Belal <yorder07@gmail.com>
2024-09-20 03:08:23 +0600

    Refactor code to optimize distance calculation in A_Greg_and_Array.cpp

--- A_Greg_and_Array.cpp
+++ A_Greg_and_Array.cpp
@@ -3,7 +3,7 @@
 //                                                                    ||
 //                                                                    ||
 //      BANGLADESH  UNIVERSITY OF BUSINESS AND TECHNOLOGY             ||
-//                          INTAKE 52                                 ||
+//                          llAKE 52                                 ||
 //                      DEPARTMENT OF CSE                             ||
 //                                                                    ||
 //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -11,11 +11,11 @@
 using namespace std;
 
 typedef long long ll;
-typedef vector<int> vi;
+typedef vector<ll> vi;
 typedef vector<ll> vl;
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
-typedef pair<int, int> pii;
+typedef pair<ll, ll> pii;
 typedef pair<double, double> pdd;
 typedef pair<ll, ll> pll;
 typedef vector<pii> vii;
@@ -28,15 +28,15 @@ typedef double dl;
 #define MP make_pair
 #define endl '\n'
 #define all(a) (a).begin(), (a).end()
-#define sz(x) (int)x.size()
+#define sz(x) (ll)x.size()
 #define mid(l, r) ((r + l) / 2)
 #define left(node) (node * 2)
 #define right(node) (node * 2 + 1)
-#define mx_int_prime 999999937
+#define mx_ll_prime 999999937
 
 const double PI = acos(-1);
 const double eps = 1e-9;
-const int inf = 2000000000;
+const ll inf = 2000000000;
 const ll infLL = 9000000000000000000;
 #define MOD 1000000007
 
@@ -56,13 +56,13 @@ const ll infLL = 9000000000000000000;
     freopen("inputf.in", "r", stdin); \
     freopen("outpuft.out", "w", stdout);
 
-typedef vector<int>::iterator vit;
-typedef set<int>::iterator sit;
+typedef vector<ll>::iterator vit;
+typedef set<ll>::iterator sit;
 
-int dx[] = {0, 0, +1, -1};
-int dy[] = {+1, -1, 0, 0};
-// int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
-// int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+ll dx[] = {0, 0, +1, -1};
+ll dy[] = {+1, -1, 0, 0};
+// ll dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+// ll dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
 
 template <typename F, typename S>
 ostream &operator<<(ostream &os, const pair<F, S> &p)
@@ -135,9 +135,9 @@ void faltu()
 }
 
 template <typename T>
-void faltu(T a[], int n)
+void faltu(T a[], ll n)
 {
-    for (int i = 0; i < n; ++i)
+    for (ll i = 0; i < n; ++i)
         cerr << a[i] << ' ';
     cerr << endl;
 }
@@ -152,15 +152,63 @@ void faltu(T arg, const hello &...rest)
 int main()
 {
     optimize();
-    int tc = 1;
+    ll tc = 1;
     // cin >> tc;
     while (tc--)
     {
-        int n, op, q;
+        ll n, op, q;
         cin >> n >> op >> q;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++)
+        vector<ll> a(n + 1, 0);
+        for (ll i = 0; i < n; i++)
+        {
             cin >> a[i];
+        }
+        vector<pair<ll, ll>> pr(op);
+        vector<ll> val(op);
+        for (ll i = 0; i < op; i++)
+        {
+            ll l, r, d;
+            cin >> l >> r >> d;
+            pr[i] = {l, r};
+            val[i] = d;
+        }
+        vector<ll> o_sum(op + 1, 0);
+        while (q--)
+        {
+            ll l, r;
+            cin >> l >> r;
+            o_sum[l - 1]++;
+            o_sum[r]--;
+        }
+        for (ll i = 1; i < op; i++)
+        {
+            o_sum[i] += o_sum[i - 1];
+        }
+
+        vector<ll> ans(n + 1, 0);
+        for (ll i = 0; i < op; i++)
+        {
+            if (o_sum[i])
+            {
+                ll x = pr[i].first;
+                ll y = pr[i].second;
+                ans[x - 1] += o_sum[i] * val[i];
+                ans[y] -= o_sum[i] * val[i];
+            }
+        }
+        for (ll i = 1; i <=n; i++)
+        {
+            ans[i] += ans[i - 1];
+        }
+        for (ll i = 0; i < n; i++)
+        {
+            a[i] += ans[i];
+        }
+        for (ll i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
