cff66388a350ee9fc8c75b9f7eda23506c5cbaa8
Anas Ibn Belal <yorder07@gmail.com>
2025-01-06 13:05:30 +0600

    Refactor C_Odd_Even_Increments implementation for improved clarity and efficiency

--- C_Odd_Even_Increments.cpp
+++ C_Odd_Even_Increments.cpp
@@ -151,27 +151,28 @@ int main()
     cin >> tc;
     while (tc--)
     {
-        int n;cin>>n;
-        vector<int>v(n);
-        for(int i=0;i<n;i++) cin>>v[i];
-        bool check=true;
-        int a=v[0],b=v[1];
-        for (int i = 0; i < n; i+=2)
-        {
-            if(a!=v[i]){
-                check=false;
-                break;
+        int n; cin >> n;
+        vector<int> a(n);
+
+        int even1 = 0, even2 = 0, odd1 = 0, odd2 = 0;
+        for(int i = 0; i < n; ++i) {
+            cin >> a[i];
+            if(i % 2 == 0) {
+                if(a[i] % 2 == 1) odd1 = 1;
+                else even1 = 1; 
+            } else {
+                if(a[i] % 2 == 1) odd2 = 1;
+                else even2 = 1;
             }
         }
-        for (int i = 1; i < n && check; i+=2)
-        {
-            if(b!=v[i]){
-                check=false;
-                break;
-            }
+
+        if(even1 && odd1) {
+            cout << "NO\n";
+        } else if(even2 && odd2) {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
         }
-        if(check) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
     }
 
     return 0;
