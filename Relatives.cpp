a6588fafca75e7e3c8e56486d36c3ca9c6c41a1e
Anas Ibn Belal <anasibnebelal400@gmail.com>
2025-09-27 03:39:01 +0600

    Eular totient and Phi Function

---
+++ Relatives.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int n;
+    while(cin>>n){
+        if(n==0) break;
+        if(n==1){
+            cout<<0<<endl;
+            continue;
+        }
+       // cout<<n<<endl;
+        ll ans=n;
+        for (int i = 2; i*i <=n; i++)
+        {
+            if(n%i==0){
+                while(n%i==0){
+                    n/=i;
+                }
+                ans-=ans/i;
+            }
+        }
+        if(n>1) ans-=ans/n;
+        cout<<ans<<endl;
+        
+    }
+    return 0;
+}
