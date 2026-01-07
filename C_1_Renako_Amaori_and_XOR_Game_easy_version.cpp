6690e099ea886fa20f24a1d06b69a3077891d95a
anas20023 <anasibnebelal400@gmail.com>
2026-01-07 23:33:10 +0600

    Some Hehe Accepted !

---
+++ C_1_Renako_Amaori_and_XOR_Game_easy_version.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        vector<int>a(n),b(n);
+        int x=0,y=0;
+        for (int i = 0; i < n; i++)
+        {
+            cin>>a[i];
+            // if(a[i]==0)x++;
+            // else y++;
+            x^=a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin>>b[i];
+            // if(b[i]==0)p++;
+            // else q++;
+            x^=b[i];
+        }
+        if(!x){
+            cout<<"Tie"<<endl;
+            continue;
+        }
+        // int ansa=abs(x-y),ansb=abs(p-q);
+        // cout<<ansa<<" "<<ansb<<endl;
+        for (int i = 0; i < n; i++)
+        {
+            if(a[i]^b[i]){
+                y=i;
+            }
+        }
+        if(y&1) cout<<"Mai"<<endl;
+        else cout<<"Ajisai"<<endl;
+        
+        
+        
+    }
+    return 0;
+}
